ae8d36d52029a97da8d90045173920f6ab2ed7d3
yiqing <hsdxyq@gamil.com>
2023-05-16 14:34:42 +0200

    conf

--- soup/conf.py
+++ soup/conf.py
@@ -1,7 +1,7 @@
 proj_dir = '/Users/huyiqing/PycharmProjects/UW_lab/web_scrapt/project'  # project direction
 begin_date = ''  # empty means from today, otherwise use YYYYMMDD date format
-end_date = '20230506'  # empty means end at 20221031 when is AI begins
+end_date = '20230516'  # empty means end at 20221031 when is AI begins, this end_date will be crawled too
 tops = 50  # to control how many top pages per day you want to crawl
-is_ai = False  # to crawl pictures generate by AI or not
+is_ai = True  # to crawl pictures generate by AI or not
 redis_host = 'localhost'
 redis_db = 0
